39943a746cfa0dbd7e4142c45c0477ce84aabdcc
Olivier Laurent <olt@tinyerp.com>
2009-04-08 09:42:51 +0200

    Styling the comment boxes

--- source/.static/tiny_js_kit.js
+++ source/.static/tiny_js_kit.js
@@ -1,38 +1,20 @@
 
 $(document).ready(function(){
-  var a = $('div[path].js-kit-comments').css(
-  {
-    'border': '1px solid black',
-    'background-color': '#e6e6e6',
-    'position': 'relative',
-    'float': 'right',
-    'padding': '2px',
-    'top': '-40px',
-    'right': '-10px',
-    'width': '25%',
-  }).find('a').css(
-  {
-    'color': '#444',
-    'font-size': '10pt',
-  }).length;
-
-alert(a);
-  
-//  $('.js-poweredBy').find('a').css(
-//  {
-//    'font-size': '7pt',
-//  })
-//  
-//  $('div[permalink].js-kit-comments').css(
-//  {
-//    'border': '1px solid black',
-//    'background-color': '#e6e6e6',
-//    'padding': '2px',
-//  }).find('a').css(
-//  {
-//    'color': '#444',
-//    'font-size': '10pt',
-//  })
-//  
+  $('div.js-kit-comments').each(function(i, elem) {
+    var jelem = $(elem);
+    if (jelem.attr('permalink') != null) {
+      jelem.css(
+        {
+          'position': 'static',
+          'float': 'left',
+          'top': '0px',
+          'right': '0px',
+          'width': '25%',
+        }
+      );
+      jelem.after('<br />');
+    }
+  }
+  );
 });
 
